9d25c282ceee0f757a8dc53730dd7c76b2ccdfce
MasatoMakino <unagiinu.g@gmail.com>
2020-10-22 12:49:09 +0900

    update : devDependencies.

--- src/TestNewTween.ts
+++ src/TestNewTween.ts
@@ -1,8 +1,8 @@
-import TWEEN from "@tweenjs/tween.js";
+import {Tween} from "@tweenjs/tween.js";
 import {BoxGeometry, Mesh, MeshNormalMaterial} from "three";
 
 const geometry = new BoxGeometry( 0.2, 0.2, 0.2 );
 const material = new MeshNormalMaterial();
 const cube = new Mesh(geometry, material)
-const tween = new TWEEN.Tween(cube.position).to({x: 100, y: 100, z: 100}, 10000); // NG
-const tweenAny = new TWEEN.Tween(cube.position as any).to({x: 100, y: 100, z: 100}, 10000); // OK
+const tween = new Tween(cube.position).to({x: 100, y: 100, z: 100}, 10000); // NG
+const tweenAny = new Tween(cube.position as any).to({x: 100, y: 100, z: 100}, 10000); // OK
